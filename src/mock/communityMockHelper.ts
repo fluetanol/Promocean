44af07c6f255987ff4e2d99c0470a0436453fd4a
Jung TaeSeung <mike415415@naver.com>
2026-01-30 17:56:36 +0900

    fix, test : vercel환경 환경변수 동작 여부 확인 용 fix and test

--- src/mock/communityMockHelper.ts
+++ src/mock/communityMockHelper.ts
@@ -1,5 +1,7 @@
 ﻿// Community mock helpers
 import { CommunityBoardItemProps, CommunityFloatingItemProps, CommunityPostItemResponse } from "@/types/itemType";
+import { usePathname } from "next/navigation";
+import { promises as fs } from 'fs';
 
 export function makeMockPostPopular(count = 10): CommunityFloatingItemProps[] {
   return Array.from({ length: count }, (_, i) => ({
@@ -38,9 +40,16 @@ export async function makeMockPostDetail(
   console.log("makeMockPostDetail called with postId:", postId);
 
   const isServer = typeof window === "undefined";
+  let isVercel:boolean = process.env.VERCEL !== undefined;
 
+  console.log("isVercel:",  isVercel);
+
+  
+  //버셀은 자체적으로 VERCEL_URL이라는 환경변수를 제공하는데,
+  //이걸 사용하면 배포 환경에서는 정확한 호스트네임을 가져올 수 있다.
+  //아무래도 버셀에 Production과 Preview 환경이 있어서 서버 주소가 다 다르다보니 이걸 활용하는게 
   const baseUrl = isServer
-    ? process.env.NEXT_PUBLIC_MOCK_URL || "http://localhost:3000"
+    ?  ( isVercel ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000")
     : "";
 
   const url = isServer
@@ -54,7 +63,7 @@ export async function makeMockPostDetail(
     console.error("Failed to fetch mock data:", res.status);
     return null;
   }
-
+;
   const list = (await res.json()) as CommunityPostItemResponse[];
 
   if (!Array.isArray(list) || list.length === 0) {
