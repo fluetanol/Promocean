df7505fbfc1c2b3955e6f425df7ec3a6ce45d614
Jung TaeSeung <mike415415@naver.com>
2026-03-23 13:50:10 +0900

    feat : team space list mock api 개발

--- src/api/mock_space/space.ts
+++ src/api/mock_space/space.ts
@@ -1,6 +1,7 @@
 import { ApiResponse } from "@/types/apiTypes/common";
-import { GetSpaceArchiveFoldersResponse } from "@/types/apiTypes/space";
+import { GetSpaceArchiveFoldersResponse, GetTeamSpaceListResponse } from "@/types/apiTypes/space";
 import { apiFetch } from "../fetcher";
+import { TeamSpaceChoiceItemProps } from "@/components/item/TeamSpaceTeamChoiceItem";
 
 
 
@@ -21,6 +22,19 @@ const MockSpaceAPI = {
         }
     },
 
+    async getMockTeamSpaceListData() : Promise<GetTeamSpaceListResponse | null> {
+        try {
+            const response = await fetch('/mock/TeamSpaceListResponse.json');
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const data: GetTeamSpaceListResponse = await response.json();
+            return data;
+        } catch (error) {
+            console.error('Mock 데이터 로딩 실패:', error);
+            return null;
+        }
+    }
 
 }
 
