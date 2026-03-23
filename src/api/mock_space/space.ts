6d376302703b809f16b91f9b7f7701627546a8bd
Jung TaeSeung <mike415415@naver.com>
2026-03-23 17:13:18 +0900

    feat  :team space participants mock data api 연동

--- src/api/mock_space/space.ts
+++ src/api/mock_space/space.ts
@@ -2,6 +2,7 @@ import { ApiResponse } from "@/types/apiTypes/common";
 import { GetSpaceArchiveFoldersResponse, GetTeamSpaceListResponse } from "@/types/apiTypes/space";
 import { apiFetch } from "../fetcher";
 import { TeamSpaceChoiceItemProps } from "@/components/item/TeamSpaceTeamChoiceItem";
+import { getSpaceParticipantsResponse } from "../space";
 
 
 
@@ -30,6 +31,26 @@ const MockSpaceAPI = {
             }
             const data: GetTeamSpaceListResponse = await response.json();
             return data;
+        } catch (error) {
+            console.error('Mock 데이터 로딩 실패:', error);
+            return null;
+        }
+    },
+
+    async getMockSpaceParticipants(spaceId : number) : Promise< getSpaceParticipantsResponse | null> {
+        try {
+            const response = await fetch(`/mock/TeamSpaceParticipantsResponse.json`);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const data = await response.json();
+            
+            const filteredParticipants : getSpaceParticipantsResponse = data[spaceId] || { participants : [] };
+
+            console.log(`Mock 데이터에서 spaceId ${spaceId}에 해당하는 참가자 목록:`, filteredParticipants);
+
+            return filteredParticipants;
+            
         } catch (error) {
             console.error('Mock 데이터 로딩 실패:', error);
             return null;
